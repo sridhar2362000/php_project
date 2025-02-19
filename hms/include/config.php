b9228119d3f1d00a494e58b70f3070d1957d2aac
sridhar2362000 <63249840+sridhar2362000@users.noreply.github.com>
2025-02-19 21:01:23 +0530

    Update config.php passwords

--- hms/include/config.php
+++ hms/include/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','hmsdb.cx8qgkoc6txa.us-east-1.rds.amazonaws.com');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','admin');
+define('DB_PASS' ,'hmsdb123');
 define('DB_NAME', 'hms');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
