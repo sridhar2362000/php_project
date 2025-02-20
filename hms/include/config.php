64b4eb9e574284de3645e557f440d33163ec62c6
sridhar2362000 <63249840+sridhar2362000@users.noreply.github.com>
2025-02-20 21:11:30 +0530

    Update config.php

--- hms/include/config.php
+++ hms/include/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','hmsdb.cx8qgkoc6txa.us-east-1.rds.amazonaws.com');
 define('DB_USER','admin');
 define('DB_PASS' ,'hmsdb123');
-define('DB_NAME', 'hms');
+define('DB_NAME', 'hmsDB');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
